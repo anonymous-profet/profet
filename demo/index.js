f022aead5256fe0b54191da0773cb4975cc64753
anonymous-profet <91931728+anonymous-profet@users.noreply.github.com>
2021-10-05 15:23:01 +0900

    Add files via upload

--- demo/index.js
+++ demo/index.js
@@ -1,16 +1,20 @@
 $(function(){
-    if (location.href.includes('index')){
-        document.querySelector('button[name=index]').style.borderBottom = '3px solid #fff'
-        document.querySelector('button[name=index]').style.color = '#fff'
-    }else if(location.href.includes('demo')){
-        document.querySelector('button[name=demo]').style.borderBottom = '3px solid #fff'
-        document.querySelector('button[name=demo]').style.color = '#fff'
-    }else if(location.href.includes('contact')){
-        document.querySelector('button[name=contact]').style.borderBottom = '3px solid #fff'
-        document.querySelector('button[name=contact]').style.color = '#fff'
-    }else if(location.href.includes('document')){
-        document.querySelector('button[name=documentation]').style.borderBottom = '3px solid #fff'
-        document.querySelector('button[name=documentation]').style.color = '#fff'
+    let btn = document.querySelector('button[name=index]')
+    if (location.href.includes('index.html')){
+        btn.style.borderBottom = '3px solid #fff'
+        btn.style.color = '#fff'
+    }else if(location.href.includes('demo.html')){
+        btn = document.querySelector('button[name=demo]')
+        btn.style.borderBottom = '3px solid #fff'
+        btn.style.color = '#fff'
+    }else if(location.href.includes('contact.html')){
+        btn = document.querySelector('button[name=contact]')
+        btn.style.borderBottom = '3px solid #fff'
+        btn.style.color = '#fff'
+    }else if(location.href.includes('documentation.html')){
+        btn = document.querySelector('button[name=documentation]')
+        btn.style.borderBottom = '3px solid #fff'
+        btn.style.color = '#fff'
     }
 });
 function navBarClick(event){
