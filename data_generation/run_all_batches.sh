c49e8811839770c73eadfa0b5f3de7e0e0fadcf7
anonymous-profet <91931728+anonymous-profet@users.noreply.github.com>
2022-01-24 20:12:03 +0900

    Update run_all_batches.sh

--- data_generation/run_all_batches.sh
+++ data_generation/run_all_batches.sh
@@ -1,14 +1,14 @@
 #!bin/bash
 INSTANCE_TYPE=$1
 
-sudo bash ./run_all_workload.sh $INSTANCE_TYPE profiling 16
-sudo bash ./run_all_workload.sh $INSTANCE_TYPE profiling 32
-sudo bash ./run_all_workload.sh $INSTANCE_TYPE profiling 64
-sudo bash ./run_all_workload.sh $INSTANCE_TYPE profiling 128
-sudo bash ./run_all_workload.sh $INSTANCE_TYPE profiling 256
+sudo bash /home/ubuntu/profet/data_generation/run_all_workload.sh $INSTANCE_TYPE profiling 16
+sudo bash /home/ubuntu/profet/data_generation/run_all_workload.sh $INSTANCE_TYPE profiling 32
+sudo bash /home/ubuntu/profet/data_generation/run_all_workload.sh $INSTANCE_TYPE profiling 64
+sudo bash /home/ubuntu/profet/data_generation/run_all_workload.sh $INSTANCE_TYPE profiling 128
+sudo bash /home/ubuntu/profet/data_generation/run_all_workload.sh $INSTANCE_TYPE profiling 256
 
-sudo bash ./run_all_workload.sh $INSTANCE_TYPE latency 16
-sudo bash ./run_all_workload.sh $INSTANCE_TYPE latency 32
-sudo bash ./run_all_workload.sh $INSTANCE_TYPE latency 64
-sudo bash ./run_all_workload.sh $INSTANCE_TYPE latency 128
-sudo bash ./run_all_workload.sh $INSTANCE_TYPE latency 256
+sudo bash /home/ubuntu/profet/data_generation/run_all_workload.sh $INSTANCE_TYPE latency 16
+sudo bash /home/ubuntu/profet/data_generation/run_all_workload.sh $INSTANCE_TYPE latency 32
+sudo bash /home/ubuntu/profet/data_generation/run_all_workload.sh $INSTANCE_TYPE latency 64
+sudo bash /home/ubuntu/profet/data_generation/run_all_workload.sh $INSTANCE_TYPE latency 128
+sudo bash /home/ubuntu/profet/data_generation/run_all_workload.sh $INSTANCE_TYPE latency 256
